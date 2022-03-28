f6614b36f7624a18c0bc180f2a1684a8de9afdae
Robert Sheehan <robertnsheehan@gmail.com>
2022-03-28 17:10:24 +0100

    Updated lin_alg
    Added single func for solving Ax=b via LU decomp. Also tested the code.

--- Integral_Equations/Integral_Equations/Test_Routines.cpp
+++ Integral_Equations/Integral_Equations/Test_Routines.cpp
@@ -52,12 +52,12 @@ void testing::ludcmp_test()
 	// Ensure the LU Decomposition scheme is working correctly
 	// R. Sheehan 4 - 10 - 2021
 
-	int nrows = 4, ncols = 4, ncolsb = 1; 
-	double d = 0.0; 
+	int nrows = 4, ncols = 4, ncolsb = 1;
+	double d = 0.0;
 	std::vector<std::vector<double>> A;
 	std::vector<std::vector<double>> Ainv;
 	std::vector<double> b(nrows);
-	std::vector<int> indx(nrows); 
+	std::vector<int> indx(nrows);
 
 	A = vecut::array_2D(nrows, ncols);
 	Ainv = vecut::array_2D(nrows, ncols);
@@ -80,24 +80,34 @@ void testing::ludcmp_test()
 	std::cout << "\nVector b is\n";
 	vecut::print_vec(b);
 
-	lin_alg::ludcmp(A, nrows, indx, d); 
-	lin_alg::lubksb(A, nrows, indx, b); 
+	bool TEST_LU_SLV = true;
+
+	if (TEST_LU_SLV) {
+		lin_alg::luslv(A, nrows, b);
+	}
+	else {
+		lin_alg::ludcmp(A, nrows, indx, d);
+		lin_alg::lubksb(A, nrows, indx, b);
+	}
 
 	std::cout << "\nLU decomposition of A is\n";
 	vecut::print_mat(A);
 
 	std::cout << "\nSolution of system of equations is\n";
 	vecut::print_vec(b);
-	
-	lin_alg::ludet(A, d, nrows); 
 
-	std::cout << "\nDeterminant of the Matrix A is "<<d<<"\n";
+	if (!TEST_LU_SLV) {
 
-	lin_alg::luinv(A, Ainv, indx, nrows); 
+		lin_alg::ludet(A, d, nrows);
 
-	std::cout << "\nInverse of the Matrix A is\n"; 
-	vecut::print_mat(Ainv);
-	std::cout << "\n";
+		std::cout << "\nDeterminant of the Matrix A is " << d << "\n";
+
+		lin_alg::luinv(A, Ainv, indx, nrows);
+
+		std::cout << "\nInverse of the Matrix A is\n";
+		vecut::print_mat(Ainv);
+		std::cout << "\n";
+	}
 
 	A.clear(); b.clear(); Ainv.clear(); indx.clear(); 
 }
